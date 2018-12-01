5f9c2af31b4e5ea1ddfcab13c9ecd866e376716c
沈唁 <52o@qq52o.cn>
2018-12-01 17:23:39 +0800

    Update wordpress-qcloud-cos.php

--- wordpress-qcloud-cos.php
+++ wordpress-qcloud-cos.php
@@ -3,7 +3,7 @@
 Plugin Name: 腾讯云对象存储服务COS
 Plugin URI: https://qq52o.me/2130.html
 Description: 使用腾讯云对象存储服务 COS 作为附件存储空间。（This is a plugin that uses QCloud Cloud Object Service for attachments remote saving.）
-Version: 1.4
+Version: 1.4.1
 Author: sy-records
 Author URI: https://qq52o.me
 License: GPL v3
