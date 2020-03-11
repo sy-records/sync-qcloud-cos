bb8f6e045e3e706628bcb20bf6a817b4f7de76e3
sy-records <52o@qq52o.cn>
2020-03-11 15:01:14 +0800

    update version to 1.6.4

--- wordpress-qcloud-cos.php
+++ wordpress-qcloud-cos.php
@@ -3,7 +3,7 @@
 Plugin Name: Sync QCloud COS
 Plugin URI: https://qq52o.me/2518.html
 Description: 使用腾讯云对象存储服务 COS 作为附件存储空间。（This is a plugin that uses Tencent Cloud Cloud Object Storage for attachments remote saving.）
-Version: 1.6.3
+Version: 1.6.4
 Author: 沈唁
 Author URI: https://qq52o.me
 License: Apache 2.0
@@ -13,7 +13,7 @@ require_once 'cos-sdk-v5/vendor/autoload.php';
 
 use Qcloud\Cos\Client;
 
-define('COS_VERSION', "1.6.3");
+define('COS_VERSION', "1.6.4");
 define('COS_BASEFOLDER', plugin_basename(dirname(__FILE__)));
 
 // 初始化选项
