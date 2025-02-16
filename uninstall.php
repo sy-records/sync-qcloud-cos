654f18fddb0838dd2bfdbf5678c0fbef39ad6e64
Luffy <52o@qq52o.cn>
2025-02-16 10:13:10 +0800

    Optimize uninstall code (#76)

--- uninstall.php
+++ uninstall.php
@@ -4,15 +4,9 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 
-$cos_options = get_option('cos_options', true);
-$upload_url_path = get_option('upload_url_path');
-$cos_upload_url_path = esc_attr($cos_options['upload_url_path']);
+update_option('upload_url_path', '');
 
-if ($upload_url_path == $cos_upload_url_path) {
-    update_option('upload_url_path', '');
-}
-
-$cos_delete_options = esc_attr($cos_options['delete_options']);
-if ($cos_delete_options == 'true') {
+$cos_options = get_option('cos_options', ['delete_options' => 'true']);
+if (esc_attr($cos_options['delete_options']) == 'true') {
     delete_option('cos_options');
 }
